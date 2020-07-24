25db66b2618cd3c1e2a4caf71472e5dc42ee648c
vansinhit <transinh631@gmail.com>
2020-07-24 17:01:59 +0700

    sinh datePicker

---
+++ app/src/main/java/com/example/booking_room/DatePicker.kt
@@ -0,0 +1,12 @@
+package com.example.booking_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatePicker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_date_picker)
+    }
+}
